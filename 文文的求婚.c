0ea54450c3b624aa2e2b4f709922928c99c79110
syuan0327 <47874861+syuan0327@users.noreply.github.com>
2020-08-16 17:14:04 +0800

    Update 文文的求婚.c

--- 文文的求婚.c
+++ 文文的求婚.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-int year,s;
+int year,s1,s2,s3;
 int main(){
-    while(1){
-        printf("please input year:\n");
-        scanf("%d",&year);
-        s=year%400;
-        if(s==0)printf("»Ó¦~\n");
+    while(scanf("%d",&year)!=EOF){
+        s1=year%400;
+        s2=year%4;
+        s3=year%100;
+
+        if(s1==0||(s2==0&s3!=0))printf("é–å¹´\n");
         else if(year<400)printf("error!\n");
-        else printf("¥­¦~\n");
+        else printf("å¹³å¹´\n");
     }
-
-
 }
